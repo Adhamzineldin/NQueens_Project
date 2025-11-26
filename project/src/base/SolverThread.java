6f2f479ff3526c223574c75e5dbd75fcb38413af
Adham Zineldin <Mohalya3@gmail.com>
2025-11-26 22:48:24 +0200

    refactor: remove unused variables and clean up code in SolverThread

--- project/src/base/SolverThread.java
+++ project/src/base/SolverThread.java
@@ -2,14 +2,13 @@ package base;
 
 public class SolverThread extends Thread {
 
-    private final int startingColumn;
-    private final Board board;
     private final NQueenSolver solver;
     private volatile boolean stop = false;
 
     public SolverThread(int n, int col, StepListener listener) {
-        this.startingColumn = col;
-        this.board = new Board(n);
+//        this.startingColumn = col;
+        //    private final int startingColumn;
+        Board board = new Board(n, col);
         this.solver = new NQueenSolver(board);
         solver.setListener(listener);
     }
@@ -17,8 +16,7 @@ public class SolverThread extends Thread {
     @Override
     public void run() {
         // place queen in row 0
-        board.place(0, startingColumn);
-
+//        board.place(0, startingColumn);
         // backtracking from row 1
         solver.solveFromRow(1);
     }
