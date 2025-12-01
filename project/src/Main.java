ce77e9ad7c7358ea2514386265ffcc71712e42b8
Adham Zineldin <Mohalya3@gmail.com>
2025-12-01 17:53:12 +0200

    implement app lock

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,12 +1,22 @@
 import gui.GUI;
-import javax.swing.SwingUtilities;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
 
 public class Main {
-    public static void main(String[] args) {
-        // Launch the amazing GUI!
-        SwingUtilities.invokeLater(() -> {
-            GUI gui = new GUI();
-            gui.setVisible(true);
+    public static void main(String[] args) throws Exception {
+        RandomAccessFile file = new RandomAccessFile(new File("app.lock"), "rw");
+        FileLock lock = file.getChannel().tryLock();
+        if (lock == null) {
+            System.out.println("Another instance is running!");
+            System.exit(0);
+        }
+
+        // Run your GUI
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new GUI().setVisible(true);
         });
+        
     }
 }
